57d36182f499fe1949a313a250e2ceeb954844bc
ricklon <rick.rickanderson@gmail.com>
2016-09-14 19:32:25 -0400

    Full Auto commands are added.

--- arduino/OTTOFullAutoDrive/OTTOFullAutoDrive.ino
+++ arduino/OTTOFullAutoDrive/OTTOFullAutoDrive.ino
@@ -8,7 +8,7 @@
 #include <SoftPWMServo.h>
 #include "MPU9250.h"
 
-
+#define DEBUG_SERIAL true
 #define MAX_CMD_BUF  40
 
 //Setup RC Controller
@@ -32,7 +32,24 @@ int chmin[4];
 int chmid[4];
 int chmax[4];
 
+
 /*
+ * Automatic Set Up
+ */
+
+#define MAX_CMD_BUF  20
+#define CMD_STR 0
+#define CMD_DIR 1
+#define CMD_GAS 2
+#define CMD_TIME 3
+
+unsigned long last_time;
+
+
+
+/*
+ * 
+ * 
    RC Controller states
     out of range or off
     kill
@@ -202,87 +219,178 @@ void doAction() {
   }//end kill switch
 
   //check if auto on
-if  if (ch[3] > 1500 ) {
-    //auto mode is on
-    doAutoCommands();
-    return;
-  }
-  else if (ch[0] == 0 ) //check for RCCommands
+  if (ch[3] > 1500 ) {
+      //auto mode is on
+      //Check if command waiting
+      if (Serial.available() > 0) {
+        doAutoCommands();
+      }
+      return;
+    }
+    else if (ch[0] == 0 ) //check for RCCommands
+    {
+      Serial.printf("Out of Range or Powered Off\n");
+      //set brake
+      //kill the machine
+    }
+    else
+    {
+      /*
+         steering 1100 - 1500 map left
+         steering between 1500 - 1600 straight
+          steering 1600 - 1900 map left
+      */
+      setSteering(ch[0]);
+      /*
+         Throttling:
+          1100 - 1500: reverse
+          1500 - 1600: no throttle
+          1600 - 1900: forward
+      */
+      setThrottle(ch[1]);
+    }
+}
+
+
+
+void autoSteer(int str) //0 - 255
+{
+  int pos;
+  if (str < 124 ) //steer right
   {
-    Serial.printf("Out of Range or Powered Off\n");
-    //set brake
-    //kill the machine
+    pos = map(str, 127, 0, 1525, 1589);
+  }
+  else if (str > 130) //steer left
+  {
+    pos = map(str, 130, 255, 1550, 1400);
   }
   else
   {
-    /*
-       steering 1100 - 1500 map left
-       steering between 1500 - 1600 straight
-        steering 1600 - 1900 map left
-    */
-    setSteering(ch[0]);
-    /*
-       Throttling:
-        1100 - 1500: reverse
-        1500 - 1600: no throttle
-        1600 - 1900: forward
-    */
-    setThrottle(ch[1]);
+    pos = 1500;
   }
+
+
+  // SoftPWMServoServoWrite(PIN_STR, pos);
+  Serial.printf("str: ch: %d servo: %d\n ", str, pos);
+  delay(25);
 }
+
+void autoThrottle(int DIR, int thr) {
+
+  //shoot through protection
+  if ( DIR != PREV_DIR) {
+    delay(SHOOT_DELAY);
+  }
+  PREV_DIR = DIR;
+
+  digitalWrite(PIN_M1_DIR, DIR);
+  digitalWrite(PIN_M2_DIR, DIR);
+  SoftPWMServoPWMWrite(PIN_M1_PWM, thr); //these aren't servos use pwm
+  SoftPWMServoPWMWrite(PIN_M2_PWM, thr);//these aren't servos use pwm
+  Serial.printf("thr: dir: %d, pwm: %d\n ", DIR, thr);
+  delay(25);
+}
+
+
 
 void doAutoCommands() {
   /*
-   * read entire input max length
-   * while more serial
-   * fill the comdBuf until a ','
-   * cmds
-   * 1.
-   * 2.
-   * 3.
-   * 4.
-   * 5.
-   * When new line end
-   * execute the commands
-   */
+     read entire input max length
+     while more serial
+     fill the comdBuf until a ','
+     cmds
+     Output:
+     `accel_x, accel_y, accel_z, yaw, pitch, roll,time`.
+     Input Commands:
+     `steering 0-255, direction 0/1, gas amount 0-255, timestamp`
+     1. steer 0-255 maps to
+     2. direction
+     3. gas
+     4. time stamp
+     When new line end
+     execute the commands
+
+     255,1,255,1110000
+     255,0,0,1110001
+  */
+
   //http://arduino.stackexchange.com/questions/1013/how-do-i-split-an-incoming-string
-   char cmdBuf;MAX_CMD_BUF + 1];
-   byte size = Serial.readBytes(cmdBuf, MAX_CMD_BUF);
-   cmdBuf[size] = 0;
-  
-}
+  int cmd_cnt = 0;
+  char cmdBuf [MAX_CMD_BUF + 1];
+
+  int str;
+  int dir;
+  int gas;
+  unsigned int time;
 
 
-void processCommand(const char * command) {
-   if (strcmp(command, "help") == 0 || strcmp(command, "?") == 0  ) {
-      Serial.println("mount, ls, touch, cat, unmount files.");
-   }
-}
 
-void processIncomingByte( const byte inByte) {
-  static char buf[MAX_INPUT];
-  static unsigned int ii = 0;
-  Serial.print(inByte);
-  //Serial.print(inByte,DEC);
-  //Serial.print(":");
-  switch(inByte)
-  {
-   case '\r': //terminator reached
-    buf[ii] = 0; //terminate array
-    Serial.println("");
-    processCommand(buf);
-    //gCommand = buf;
-    //reset count
-    ii = 0;
-    Serial.print("> ");
-    break;
-   case '\n':
-     break;
-   default:
-     if (ii < (MAX_INPUT - 1)) {
-      buf[ii++] = inByte; 
-     }
-     break;
+  byte size = Serial.readBytes(cmdBuf, MAX_CMD_BUF);
+  cmdBuf[size] = 0;
+  char* command = strtok(cmdBuf, ",");
+  while (command != 0) {
+    //int cmdnum = atoi(command);
+    //Serial.printf("%d, %s\n", cmd_cnt, command);
+    switch (cmd_cnt) {
+      case CMD_STR:
+        str = atoi(command);
+        if (str > 255 || str < 0) {
+          return;
+        }
+        if (DEBUG_SERIAL) {
+          Serial.printf("%d, %d\n", cmd_cnt, str);
+        }
+        break;
+      case CMD_DIR:
+        dir = atoi(command);
+        if (dir  < 0  || dir > 1) {
+          return;
+        }
+        if (DEBUG_SERIAL) {
+          Serial.printf("%d, %d\n", cmd_cnt, dir );
+        }
+        break;
+      case CMD_GAS:
+        gas = atoi(command);
+        if (gas > 255 || gas < 0) {
+          return;
+        }
+        if (DEBUG_SERIAL) {
+          Serial.printf("%d, %d\n", cmd_cnt, gas);
+        }
+        break;
+      case CMD_TIME:
+        time = atoi(command);
+        if (time < last_time) {
+          return;
+        }
+        last_time = time;
+        if (DEBUG_SERIAL) {
+          Serial.printf("%d, %lu\n", cmd_cnt, time);
+        }
+        break;
+      default:
+        if (DEBUG_SERIAL) {
+          Serial.println("NOOP");
+        }
+        return; //return if there are too many commands or non matching
+    }
+    command = strtok(0, ",");
+    cmd_cnt++;
+
+    if (cmd_cnt == 4) {
+      if (DEBUG_SERIAL) {
+        Serial.printf("str: %d, dir: %d, gas: %d, time: %lu\n", str, dir, gas, time);
+      }
+      //do commands
+      autoSteer(str);
+      autoThrottle(dir, gas);
+    }
+  }
+
+  delay(100);
+  if (DEBUG_SERIAL) {
+    Serial.printf("DONE COMMANDS: %lu\n", millis());
   }
 }
 
